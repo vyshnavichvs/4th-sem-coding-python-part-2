e43ccf5c97ca827ec2902c6ebce81cdca2c50dce
CHVS Vyshnavi <133223874+vyshnavichvs@users.noreply.github.com>
2023-05-17 21:22:49 +0530

    ass_1.py

---
+++ ass_1.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def maximumSumSubarray (self,K,Arr,N):
+
+        winsum=sum(Arr[:K])
+
+        maxsum=winsum
+
+        for i in range(N-K):#0<5
+
+            winsum=winsum-Arr[i]+Arr[i+K]
+
+            maxsum=max(maxsum,winsum)
+
+        return maxsum
